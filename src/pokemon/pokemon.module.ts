0d274c01aa07298836b3beef06d1fb531b17b232
emanuelhardwell <emanuelnoe16@gmail.com>
2024-10-20 22:15:47 -0600

    feat: Create class HandleError and apply

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -3,6 +3,7 @@ import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
+import { HandleError } from 'src/utils/HandleError';
 
 @Module({
   imports: [
@@ -14,6 +15,6 @@ import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
     ]),
   ],
   controllers: [PokemonController],
-  providers: [PokemonService],
+  providers: [PokemonService, HandleError],
 })
 export class PokemonModule {}
---
+++ src/utils/HandleError.ts
@@ -0,0 +1,28 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { MongoServerError } from 'mongodb';
+
+@Injectable()
+export class HandleError {
+  constructor() {}
+
+  handleErrorService(error: unknown) {
+    console.log(error);
+
+    if (error instanceof MongoServerError) {
+      if (error.code) {
+        const message = `MongoDB Error [Code: ${error.code}]:: ${error.message || 'errorMessage unavailable'}`;
+        throw new BadRequestException(message);
+      }
+    }
+
+    if (typeof error === 'string') throw new BadRequestException(error);
+
+    if (error instanceof Error) throw new InternalServerErrorException(error);
+
+    throw new InternalServerErrorException(error);
+  }
+}
