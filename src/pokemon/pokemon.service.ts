36c06feed2959815e6433b86a6cbf67fef66554f
emanuelhardwell <emanuelnoe16@gmail.com>
2024-10-21 22:48:55 -0600

    feat: Create Service and Controller findOne

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
-import { Model } from 'mongoose';
+import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { HandleError } from 'src/utils/HandleError';
@@ -30,8 +30,30 @@ export class PokemonService {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(term: string) {
+    try {
+      let pokemon: Pokemon;
+
+      if (!isNaN(+term)) {
+        pokemon = await this.pokemonModel.findOne({ no: term });
+      }
+      if (!pokemon && isValidObjectId(term)) {
+        pokemon = await this.pokemonModel.findOne({ _id: term });
+      }
+      if (!pokemon) {
+        pokemon = await this.pokemonModel.findOne({
+          name: term.toLocaleLowerCase().trim(),
+        });
+      }
+
+      if (!pokemon) {
+        throw new NotFoundException(`Pockemon serached by Id, Name, No ${term} not found`);
+      }
+
+      return pokemon;
+    } catch (error) {
+      this.handleError.handleErrorService(error);
+    }
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
