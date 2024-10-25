3053647f4fba936eeb31c832ae9c9ee5769ff7a3
emanuelhardwell <emanuelnoe16@gmail.com>
2024-10-24 23:53:28 -0600

    feat: Update executeSeed from SeedService and apply xiosAdapter

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -3,10 +3,11 @@ import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
 import { HandleError } from 'src/utils/HandleError';
 import { PokemonModule } from 'src/pokemon/pokemon.module';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Module({
   imports: [PokemonModule],
   controllers: [SeedController],
-  providers: [SeedService, HandleError],
+  providers: [SeedService, HandleError, AxiosAdapter],
 })
 export class SeedModule {}
